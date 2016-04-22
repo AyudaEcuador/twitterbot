6e277a809565d6115a4526659be00926c64da6d2
Antonio Pardo <apardo@alabs.org>
2016-04-22 18:06:18 +0200

    ahora se guardan los tweets con el hashtag #AyudaEC en mongodb

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var Twitter = require('twitter');
 var util = require('util');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/ayudaecuador_ec');
+
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -8,9 +11,21 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
+var Tweet = mongoose.model('Tweet', { data: {} });
+
 client.stream('statuses/filter', {track: '#AyudaEC'}, function(stream) {
   stream.on('data', function(tweet) {
     console.log(tweet.text);
+    // save in database
+    var doc = new Tweet({ data: tweet });
+    doc.save(function(err) {
+      if (err) {
+        console.log(util.inspect(err));
+      } else {
+        console.log('Tweet: '+tweet.id+' inserted.');
+      }
+    });
+    // Retweet
     if (tweet.user.name != "AyudaEcuador") {
       client.post('statuses/retweet/' + tweet.id_str, function(error, tweet, response){
         if (!error) {
